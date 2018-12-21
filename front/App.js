5b2f57ca67ac828f4af6cd3014f36ff3623c2418
Louty <louty@Carolinas-Air.localdomain>
2018-12-21 13:16:08 +0100

    add mascotte

--- front/App.js
+++ front/App.js
@@ -5,11 +5,12 @@ import Home from './components/Home.js';
 import Login from './components/Login.js';
 import LoginConnect2 from "./components/LoginConnect2.js";
 import Signup from './components/Signup';
+import Search from './components/Search'
 
 export default class App extends React.Component {
   render() {
     return (
-      <Signup />
+      <Search />
     );
   }
 }
