d5fb15cbf6e7685b27e3977ec1a7e7f34371a2e7
olivier chabot <chabot.oliv@gmail.com>
2018-12-21 10:20:06 +0100

    margin Login

--- front/components/Login.js
+++ front/components/Login.js
@@ -1,21 +1,50 @@
-import React from 'react'
-import { StyleSheet, Alert, Text, TextInput, View, Image} from 'react-native'
-import Button from 'apsl-react-native-button'
-import { Navigation } from 'react-native-navigation'
+import React from "react";
+import { StyleSheet, Alert, Text, TextInput, View, Image } from "react-native";
+import Button from "apsl-react-native-button";
+import { Navigation } from "react-native-navigation";
 
 export default class Login extends React.Component {
-  handleSubmit(){
-    Alert.alert('Yay !');
+  handleSubmit() {
+    Alert.alert("Yay !");
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Image style={styles.image} source={require("../assets/logoScore.png")} />
-        <Button onPress={this.handleSubmit} style={styles.button} title="Connect to Facebook" ><Text style={styles.text}>Connect to Facebook</Text></Button>
-        <Button onPress={this.handleSubmit} style={styles.button} title="Connect to Gmail" ><Text style={styles.text}>Connect to Gmail</Text></Button>
-        <Button onPress={this.handleSubmit} style={styles.button} title="Inscription" ><Text style={styles.text}>Inscription</Text></Button>
-        <Button onPress={this.handleSubmit} style={styles.button} title="Connecxion" ><Text style={styles.text}>Connecxion</Text></Button>
+        <Image
+          style={styles.image}
+          source={require("../assets/logoScore.png")}
+        />
+        <View style={styles.containerButton}>
+          <Button
+            onPress={this.handleSubmit}
+            style={styles.button}
+            title="Connect to Facebook"
+          >
+            <Text style={styles.text}>Connect to Facebook</Text>
+          </Button>
+          <Button
+            onPress={this.handleSubmit}
+            style={styles.button}
+            title="Connect to Gmail"
+          >
+            <Text style={styles.text}>Connect to Gmail</Text>
+          </Button>
+          <Button
+            onPress={this.handleSubmit}
+            style={styles.button}
+            title="Inscription"
+          >
+            <Text style={styles.text}>Inscription</Text>
+          </Button>
+          <Button
+            onPress={this.handleSubmit}
+            style={styles.button}
+            title="Connecxion"
+          >
+            <Text style={styles.text}>Connecxion</Text>
+          </Button>
+        </View>
       </View>
     );
   }
@@ -24,24 +53,26 @@ export default class Login extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "flex-end"
+  },
+  containerButton: {
+    marginBottom: 50,
   },
   button: {
-    alignSelf:'center',
+    alignSelf: "center",
     width: 230,
     borderWidth: 1,
-    borderColor:'#33C25C',
-    margin:15,
+    borderColor: "#33C25C",
+    margin: 15
   },
   text: {
-    color:'#33C25C',
+    color: "#33C25C"
   },
   image: {
-    borderWidth:1,
     width: 320,
-    height: 75,
-    marginBottom : 120,
+    height: 82,
+    marginBottom: 120
   }
 });
